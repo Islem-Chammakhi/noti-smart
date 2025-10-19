edbe3f7a0bd6f9d2005af846b2e75068d7ebd7f4
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-10-19 16:19:57 +0100

    Major change: - Added method for OTP endpoint. It checks username and password. THEN it checks the otp code. - The login only checks if the user is valid (right username and password) and just sends encoded email

--- backend/src/main/java/com/isimm/suivi_note/services/auth/AuthenticationServiceImpl.java
+++ backend/src/main/java/com/isimm/suivi_note/services/auth/AuthenticationServiceImpl.java
@@ -1,5 +1,11 @@
-package com.isimm.suivi_note.services;
+package com.isimm.suivi_note.services.auth;
 
+import com.isimm.suivi_note.brevo.entities.BrevoOTPTemplate;
+import com.isimm.suivi_note.dto.AuthOtpLoginReq;
+import com.isimm.suivi_note.services.EmailService;
+import com.isimm.suivi_note.utils.OtpGenerator;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -7,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.isimm.suivi_note.enums.Role;
-import com.isimm.suivi_note.models.Admin;
 import com.isimm.suivi_note.models.Filiere;
 import com.isimm.suivi_note.models.Student;
 import com.isimm.suivi_note.models.User;
@@ -26,7 +31,6 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-
 public class AuthenticationServiceImpl implements AuthenticationService  {
 
     private final AuthenticationManager authenticationManager;
@@ -34,32 +38,62 @@ public class AuthenticationServiceImpl implements AuthenticationService  {
     private final UserRepo userRepository;
     private final UserIsimmRepo userIsimmRepo;
     private final PasswordEncoder passwordEncoder;
+    private final EmailService emailService;
 
     @Override
-    public AuthenticationResponse login(final AuthenticationRequest request) {
+    // This checks if the user is logged in, if so, returns true so we can go to the next step (send OTP)
+    public boolean login(final AuthenticationRequest request) {
         try{
-        final Authentication auth = this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getCin(), request.getPassword()));
+            final Authentication auth = this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getCin(), request.getPassword()));
 
-        final User user =(User) auth.getPrincipal();
-        final String accessToken = this.jwtService.generateAccessToken(user.getUsername());
-        final String refreshToken = this.jwtService.generateRefreshToken(user.getUsername());
-        System.out.println("accessToken is :"+accessToken);
-        final String tokenType="Bearer";
+            User user = (User) auth.getPrincipal();
+            if(auth.isAuthenticated()){
+                String otpCode = OtpGenerator.generateOtp();
+                emailService.sendEmail(
+                        user.getEmail(),
+                        new BrevoOTPTemplate(otpCode)
+                );
+                System.out.println("User is logged in = "+auth.isAuthenticated()+", otp="+otpCode+", sent email to "+user.getEmail());
 
-        return AuthenticationResponse.builder()
-                                     .accessToken(accessToken)
-                                     .refreshToken(refreshToken)
-                                     .tokenType(tokenType)
-                                     .build();
+                user.setOtp(passwordEncoder.encode(otpCode));
+                userRepository.save(user);
+            }
+
+            return auth.isAuthenticated();
         }catch(Exception e){
             e.printStackTrace();
             System.out.println(e.getMessage());
-            return null;
+            return false;
         }
     }
 
     @Override
+    public AuthenticationResponse loginWithOTP(AuthOtpLoginReq request) {
+        try{
+            final Authentication auth = this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.cin(), request.password()));
+            User user = (User) auth.getPrincipal();
+            if(passwordEncoder.matches(request.otp(), user.getOtp()) && auth.isAuthenticated()){
+                final String accessToken = this.jwtService.generateAccessToken(user.getUsername());
+                final String refreshToken = this.jwtService.generateRefreshToken(user.getUsername());
+                System.out.println("accessToken is :"+accessToken);
+                final String tokenType="Bearer";
+                return AuthenticationResponse.builder()
+                        .accessToken(accessToken)
+                        .refreshToken(refreshToken)
+                        .tokenType(tokenType)
+                        .build();
+            }
+            return null;
+        } catch (Exception e) {
+            System.err.println(e.getMessage()+ ", "+e.getCause());
+            return null;
+        }
+
+    }
+
+    @Override
     @Transactional
+    // TODO: If the email inside UserIsimm isn't viable, add OTP here
     public void register(final RegistrationRequest request) {
         Filiere f =new Filiere();
         f.setId("ING2_INFO");
@@ -112,3 +146,8 @@ public class AuthenticationServiceImpl implements AuthenticationService  {
     }
 
 }
+/*final User user =(User) auth.getPrincipal();
+            final String accessToken = this.jwtService.generateAccessToken(user.getUsername());
+            final String refreshToken = this.jwtService.generateRefreshToken(user.getUsername());
+            System.out.println("accessToken is :"+accessToken);
+            final String tokenType="Bearer";*/
