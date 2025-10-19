f4e4859c622155bbd9bffe245f3a73759c199396
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-10-19 16:21:43 +0100

    nothing

--- backend/src/main/java/com/isimm/suivi_note/config/security/SecurityConfig.java
+++ backend/src/main/java/com/isimm/suivi_note/config/security/SecurityConfig.java
@@ -2,12 +2,12 @@ package com.isimm.suivi_note.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -22,19 +22,20 @@ public class SecurityConfig {
     private final JWTFilter jwtFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(final HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception{
         return http
-                .cors(Customizer.withDefaults()) // Added CorsConfig
+                //.cors(Customizer.withDefaults()) // Added CorsConfig
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth ->
-                auth.requestMatchers("/api/auth/login","/api/auth/register","/api/auth/refresh","/api/userisimm").permitAll()
-                .requestMatchers("/api/admin/**").hasAuthority("ROLE_ADMIN")
-                .requestMatchers("/api/student/**").hasAuthority("ROLE_STUDENT")
-                .anyRequest().authenticated()
-            )
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .addFilterBefore(this.jwtFilter, UsernamePasswordAuthenticationFilter.class)
-            .build();
+                    auth.requestMatchers("/api/auth/**","/api/auth/register","/api/auth/refresh","/api/userisimm/**").permitAll()
+                    .requestMatchers("/api/admin/**").hasAuthority("ROLE_ADMIN")
+                    .requestMatchers("/api/student/**").hasAuthority("ROLE_STUDENT")
+                    .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(this.jwtFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .build();
     }
 
 }
