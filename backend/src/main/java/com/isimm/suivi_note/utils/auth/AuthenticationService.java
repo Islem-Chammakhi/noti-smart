ac21f5007077bcfe43b97c1e4879daa7d1221728
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-10-19 13:50:32 +0100

    Modified interface: - modfied return type of login to boolean - Added method that takes into account the otp

--- backend/src/main/java/com/isimm/suivi_note/utils/auth/AuthenticationService.java
+++ backend/src/main/java/com/isimm/suivi_note/utils/auth/AuthenticationService.java
@@ -1,12 +1,15 @@
 package com.isimm.suivi_note.utils.auth;
 
+import com.isimm.suivi_note.dto.AuthOtpLoginReq;
 import com.isimm.suivi_note.utils.auth.request.AuthenticationRequest;
 import com.isimm.suivi_note.utils.auth.request.RefreshRequest;
 import com.isimm.suivi_note.utils.auth.request.RegistrationRequest;
 import com.isimm.suivi_note.utils.auth.response.AuthenticationResponse;
+import org.springframework.http.ResponseEntity;
 
 public interface AuthenticationService {
-    AuthenticationResponse login(final AuthenticationRequest request);
+    boolean login(final AuthenticationRequest request);
+    AuthenticationResponse loginWithOTP(final AuthOtpLoginReq request);
     void register(final RegistrationRequest request);
     AuthenticationResponse refreshToken(final RefreshRequest request);
 
