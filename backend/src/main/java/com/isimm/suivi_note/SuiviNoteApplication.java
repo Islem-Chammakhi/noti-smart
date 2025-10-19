46e9c7502e0c52061cd38f18dfac45e8d5c45e85
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-10-19 16:23:46 +0100

    Commented email testing

--- backend/src/main/java/com/isimm/suivi_note/SuiviNoteApplication.java
+++ backend/src/main/java/com/isimm/suivi_note/SuiviNoteApplication.java
@@ -1,13 +1,24 @@
 package com.isimm.suivi_note;
 
+import com.isimm.suivi_note.brevo.entities.BrevoOTPTemplate;
+import com.isimm.suivi_note.brevo.service.BrevoService;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SuiviNoteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SuiviNoteApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SuiviNoteApplication.class, args);
+		/* Test Email service
+		
+		BrevoService service = context.getBean(BrevoService.class);
+
+		System.out.println("Sending email...");
+		service.sendEmail("med.yassine.kharrat@gmail.com", new BrevoOTPTemplate("12345"));*/
+
 	}
 
 }
