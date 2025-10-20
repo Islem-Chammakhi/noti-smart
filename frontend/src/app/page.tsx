f178274dd302e72eaf8cb2c5343d6f47bf2e19b1
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-10-20 20:06:55 +0100

    Added TODO

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,6 @@
+
+// TODO: If user not logged in, redirect to login/register, else redirect him to page according to role
+// This is the page loaded in / (root)
 export default function Home() {
   return <h1>Hello friends !</h1>;
 }
