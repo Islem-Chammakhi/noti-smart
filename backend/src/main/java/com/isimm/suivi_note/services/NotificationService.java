e0a7521bb34f256fbdc96fbd7746a4de4a21c676
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-10-20 20:10:36 +0100

    Added SSE Service: - Registers users in a map of SseEmitters mapped by userid (cin) - Has simple onTimeout, onError, onCompletion logic using @Slf4J - Has method to send Mark (yet to be tested)

---
+++ backend/src/main/java/com/isimm/suivi_note/services/NotificationService.java
@@ -0,0 +1,56 @@
+package com.isimm.suivi_note.services;
+
+import com.isimm.suivi_note.dto.notification.NoteDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+@Service
+public class NotificationService {
+
+    public final Map<String, SseEmitter> userEmitters = new ConcurrentHashMap<>();
+
+    public SseEmitter registerUser(String id) {
+        var emitter = new SseEmitter((long)Short.MAX_VALUE);
+
+        emitter.onTimeout(()->{
+            log.warn("onTimeout on emtter of id={}",id);
+            userEmitters.remove(id);
+        });
+        emitter.onError((e)->{
+            log.error("onError on emitter Of id={}: {}", id, e.getMessage(), e);
+            userEmitters.remove(id);
+        });
+
+        emitter.onCompletion(()->{
+            log.info("Completed emitter of id= {}",id);
+        });
+
+        userEmitters.put(id, emitter);
+
+        return emitter;
+    }
+
+    public void sendNote(NoteDTO noteDTO, String cin){
+        var emitter = userEmitters.get(cin);
+        if(emitter!=null){
+            try{
+                emitter.send(SseEmitter.event()
+                        .name("Notify note")
+                        .data(noteDTO)
+                );
+            }catch (IOException e){
+                log.error("Error sending to user id {}. message: {}", cin,e.getMessage(), e);
+            }
+        }
+        else{
+            log.warn("There's no connected user of cin ={}", cin);
+        }
+    }
+
+}
