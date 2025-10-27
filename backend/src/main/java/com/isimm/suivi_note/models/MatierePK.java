187f7f6a69481d71387c83d4453cb7e06ad6ead6
Mindblownserver <med.yassine.kharrat@gmail.com>
2025-10-27 07:10:45 +0100

    Added composite primary keys

---
+++ backend/src/main/java/com/isimm/suivi_note/models/MatierePK.java
@@ -0,0 +1,27 @@
+package com.isimm.suivi_note.models;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor
+@Embeddable
+public class MatierePK {
+    private String matiereId;
+    private UniteEnsPK ueId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof MatierePK matierePK)) return false;
+        return Objects.equals(matiereId, matierePK.matiereId) && Objects.equals(ueId, matierePK.ueId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(matiereId, ueId);
+    }
+}
